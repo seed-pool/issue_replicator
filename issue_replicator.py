c1f0bbcc2121320cdbc6e5be817beb2ebbb2f911
ShimeWataru <shime@shimewtr.me>
2021-12-29 23:00:11 +0900

    Add blank line to issue body

--- issue_replicator.py
+++ issue_replicator.py
@@ -53,7 +53,7 @@ class Replicator():
         return "{}: {}\n".format(self.repo.full_name, self.issue.html_url)
 
     def add_reference_messages(self, replicators):
-        body = self.issue.body or ""
+        body = self.issue.body + "\n" if self.issue.body else ""
         for replicator in replicators:
             if replicator.repo.full_name != self.repo.full_name:
                 body += replicator.reference_message()
